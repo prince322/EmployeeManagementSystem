cb942471de600a8b2077bdc5ca8cb2c373908bae
prince322 <52198893+prince322@users.noreply.github.com>
2021-08-24 16:34:52 +0530

    Update Assignment9Application.java

--- src/main/java/com/nagarro/javafreshertraining/Assignment9Application.java
+++ src/main/java/com/nagarro/javafreshertraining/Assignment9Application.java
@@ -11,7 +11,7 @@ public class Assignment9Application {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment9Application.class, args);
-		System.out.println("Trigerred");
+		 
 	}
 
 }
